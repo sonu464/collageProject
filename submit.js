399a5a823d9cd6fcab0f4b673f10f9a700068d94
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-04-20 09:58:08 +0530

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,65 @@
+let right = 0;
+let wrong = 0;
+let index = 0;
+let htmlQue = document.getElementById('html-que');
+let optionInputs = document.querySelectorAll('.option');
+let submit = document.getElementById("submit");
+
+function loadQuestion() {
+
+    reset();
+    const data = htmlQuestions[index];
+    // Adding Question
+    htmlQue.innerText = `${index + 1}) ${data.que}`;
+
+    // Adding Options
+    optionInputs[0].nextElementSibling.innerText = data.a;
+    optionInputs[1].nextElementSibling.innerText = data.b;
+    optionInputs[2].nextElementSibling.innerText = data.c;
+    optionInputs[3].nextElementSibling.innerText = data.d;
+    border();
+
+}
+
+loadQuestion();
+
+function submitQuiz() {
+    const data = htmlQuestions[index];
+    let ans;
+
+    optionInputs.forEach(input => {
+        if (input.checked) {
+            ans = input.value;
+            visited();
+        }
+    })
+
+    if (ans === data.correct) {
+        right++;
+        console.log("right",right);
+    } else {
+        wrong++;
+        console.log("wrong",wrong);
+    }
+
+    index++;
+    loadQuestion();
+    
+}
+
+
+function reset() {
+    optionInputs.forEach(input => {
+        input.checked = false;
+    })
+}
+
+function visited() {
+    queHtmlBtn[index].style.backgroundColor = "green";
+}
+
+
+
+
+
+
