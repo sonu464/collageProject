0b175274b5c9e47523b7623a3bfc6632df31830f
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-04-20 10:43:12 +0530

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,11 +1,19 @@
 let right = 0;
 let wrong = 0;
 let index = 0;
+let total = htmlQuestions.length;
 let htmlQue = document.getElementById('html-que');
 let optionInputs = document.querySelectorAll('.option');
 let submit = document.getElementById("submit");
 
+let navbar = document.querySelector(".navbar");
+let container = document.querySelector(".container")
+
 function loadQuestion() {
+
+    if (index === total) {
+        endQuiz();
+    }
 
     reset();
     const data = htmlQuestions[index];
@@ -58,7 +66,18 @@ function visited() {
     queHtmlBtn[index].style.backgroundColor = "green";
 }
 
-
+function endQuiz(){
+    let final1 = document.getElementById("final1")
+    navbar.style.display = "none";
+    container.style.display = "none";
+    final1.style.display= "block"
+    let div = document.createElement("div");
+    final1.appendChild(div);
+    div.className = "result"
+    div.innerHTML = `
+    <h1>Thankyou For Coming</h1>
+    <h2>Right Marks ${right}/${total} </h2> `
+}
 
 
 
