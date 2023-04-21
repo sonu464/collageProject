9338d7cc5bca0ec7171bdcc2d0b4adcb83b37a2a
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-04-21 22:12:12 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,28 @@
+function countDown() {
+    // setTimeout(() => {
+    //     navbar.style.display = "none";
+    //     container.style.display = "none";
+    //     window.location.href = "result.html"
+    // }, 3600000);
+
+    let countdown = document.getElementById("countdown")
+    let countDownDate = new Date().getTime() + 3600000;
+
+    let timer = setInterval(() => {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+
+
+        let hours = Math.floor(distance / (1000 * 60 * 60));
+        let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+        let seconds = Math.floor(distance % (1000 * 60 * 60) / 1000);
+
+        let remainingTime = ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+        countdown.textContent = remainingTime;
+
+        if (distance < 0) {
+            countdown.textContent = "Expired"
+        }
+    }, 1000);
+
+}
