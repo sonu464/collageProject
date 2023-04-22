99960eb4a2e0f25f9ac7e7b04e3283075b64017f
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-04-22 13:00:48 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,24 @@
-let mainContainer = document.getElementById("main-container");
 
-function loginBtn(){
-    setTimeout(() => {
-        window.location.replace("screen.html");
-    }, 2000);
+function loginBtn() {
+
+    let username = document.querySelector(".user").value;
+    let password = document.querySelector(".pass").value;
+    if (username === "himt" && password === "mdu") {
+        setTimeout(() => {
+            window.location.replace("screen.html");
+        }, 1000);
+    } else {
+        alert('Enter valid username and password!')
+    }
+
+
+    if (username === "himt" && password === "mdu") {
+        setTimeout(() => {
+            window.location.replace("result.html");
+        }, 5000);
+    }
+
+
 }
+
+
